07528acc5c4cc40179208a9e57094e2c9aec86a5
MTFP3 <marcus.toney.2011@gmail.com>
2025-07-01 14:16:20 -0500

    Update service worker cache version to force refresh after homepage swap

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,5 @@
 // Service Worker for 3 Ball Network PWA
-const CACHE_NAME = '3ball-network-v1.2';
+const CACHE_NAME = '3ball-network-v1.3';
 const urlsToCache = [
   '/',
   '/assets/css/style.css',
@@ -9,35 +9,32 @@ const urlsToCache = [
   '/about.html',
   '/register.html',
   '/login.html',
-  '/manifest.json'
+  '/manifest.json',
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        // Return cached version or fetch from network
-        return response || fetch(event.request);
-      }
-    )
+    caches.match(event.request).then(response => {
+      // Return cached version or fetch from network
+      return response || fetch(event.request);
+    })
   );
 });
 
-self.addEventListener('activate', (event) => {
+self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
+    caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
+        cacheNames.map(cacheName => {
           if (cacheName !== CACHE_NAME) {
             return caches.delete(cacheName);
           }
